54416e0930e780adabfde51eeaff3a8018628049
Gustavo Nep <gustavo._12@hotmail.com>
2023-08-18 11:48:51 -0300

    middlewares routes and server start

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
+// // async errors // 
+
 const express = require('express');
 const app = express();
+require('dotenv').config()
 
-app.listen(3000);
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+// middleware
+
+//routes
 
 app.get("/", (req,res) => {
-    res.write('<h1>oi</h1>')
-    console.log(req.hostname);
-    console.log("Is on your server in the route / ")
+    res.send('<h1> Store Api </h1><a href="api/v1/products"> products route </a>')
 })
+
+// products route
+
+app.use(notFoundMiddleware); // route not found
+app.use(errorMiddleware); // another errors
+
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        // connectDb
+        app.listen(port, console.log(`Server is listening to ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start();
+
+
+
