d4bfeef91656443d7f6213a4b0534979a6f9e5ba
Gustavo Nep <gustavo._12@hotmail.com>
2023-08-18 16:29:57 -0300

    controller methods products routes with router

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()
+const connectDB = require('./db/connect')
 
+const productsRouter = require('./routes/products')
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
@@ -16,17 +18,20 @@ app.get("/", (req,res) => {
     res.send('<h1> Store Api </h1><a href="api/v1/products"> products route </a>')
 })
 
+app.use('/api/v1/products', productsRouter)
+
 // products route
 
-app.use(notFoundMiddleware); // route not found
-app.use(errorMiddleware); // another errors
+app.use(notFoundMiddleware); // route not found ( se n tiver nenhuma rota existente )
 
+app.use(errorMiddleware); // usado para tratar erros de solicitações http, erros em rotas,controladores e outros middleware
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
     try {
         // connectDb
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server is listening to ${port}`))
     } catch (error) {
         console.log(error)
